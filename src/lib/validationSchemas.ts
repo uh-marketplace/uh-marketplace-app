9aff09982433b9bb3346629deeaa31cd376d86f4
BryanNak <54912705+BryanNak@users.noreply.github.com>
2025-04-18 00:21:45 -1000

    Necessary updates to write to database
    created components for adding and editing items, as well as updated dbActions.ts and validationSchemas.ts. Also updated add page and edit page to show items components rather than stuff components and updated schema.prisma as well as seed.ts and added some example items for seeding.

--- src/lib/validationSchemas.ts
+++ src/lib/validationSchemas.ts
@@ -14,3 +14,24 @@ export const EditStuffSchema = Yup.object({
   condition: Yup.string().oneOf(['excellent', 'good', 'fair', 'poor']).required(),
   owner: Yup.string().required(),
 });
+
+export const AddItemSchema = Yup.object({
+  name: Yup.string().required(),
+  condition: Yup.string().oneOf(['excellent', 'good', 'fair', 'poor']).required(),
+  price: Yup.number().positive().required(),
+  location: Yup.string().required(),
+  owner: Yup.string().required(),
+  imageUrl: Yup.string().url().required(),
+  description: Yup.string().required(),
+});
+
+export const EditItemSchema = Yup.object({
+  id: Yup.number().required(),
+  name: Yup.string().required(),
+  condition: Yup.string().oneOf(['excellent', 'good', 'fair', 'poor']).required(),
+  price: Yup.number().positive().required(),
+  location: Yup.string().required(),
+  owner: Yup.string().required(),
+  imageUrl: Yup.string().url().required(),
+  description: Yup.string().required(),
+});
