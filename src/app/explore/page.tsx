b3cf3b2e2200577b598dca45cb6c3ccf09dd7837
BryanNak <54912705+BryanNak@users.noreply.github.com>
2025-04-29 19:23:43 -1000

    Revert "Merge branch 'main' of https://github.com/uh-marketplace/uh-marketplace-app"
    This reverts commit 69bbca98a1653bae92208710344a8089f048b2c5, reversing
    changes made to b99df28b137d9f751c7f737731602badadaefa33.

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,11 +1,14 @@
-export const dynamic = 'force-dynamic';
+/* eslint-disable import/prefer-default-export */
 
 import { prisma } from '@/lib/prisma';
 import ExplorePageContent from '@/components/ExplorePage';
 
 export default async function Explore() {
+  // Fetch all items from the database using Prisma
   const items = await prisma.item.findMany({
-    orderBy: { id: 'desc' },
+    orderBy: {
+      id: 'desc', // optional: newest items first
+    },
   });
 
   return (
