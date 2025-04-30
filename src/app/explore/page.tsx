82c8a679a97d00276d7d23ac7e9994ff137a2c7a
Hargun Juneja <hargun@hawaii.edu>
2025-04-29 19:22:17 -1000

    done

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,7 +1,7 @@
-export const dynamic = 'force-dynamic';
-
 import { prisma } from '@/lib/prisma';
 import ExplorePageContent from '@/components/ExplorePage';
+
+export const dynamic = 'force-dynamic';
 
 export default async function Explore() {
   const items = await prisma.item.findMany({
