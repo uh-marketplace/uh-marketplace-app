2425667e8937be4bcf3e0c11b56e0505500f5d7b
koalanik <122417764+koalanik@users.noreply.github.com>
2025-05-01 16:25:21 -1000

    oiuygfdzfxghjk

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,3 +1,12 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable react/no-unused-prop-types */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable react/button-has-type */
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable max-len */
+/* eslint-disable react/no-unstable-nested-components */
+
 import { test, expect, type Page } from '@playwright/test';
 
 // Helper functions - defined once at the top
