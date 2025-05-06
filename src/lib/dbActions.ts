152cedeba96e64579bbe59d7a2f0c3dce8248bc3
BryanNak <54912705+BryanNak@users.noreply.github.com>
2025-05-05 23:44:52 -1000

    removed unnecessary code/files
    removed all "Stuff" files leftover from template as well as code related to "Stuff" in other files (dbActions.ts, schema.prisma, validationSchemas.ts). Removed unused next.js svgs as well

--- src/lib/dbActions.ts
+++ src/lib/dbActions.ts
@@ -1,35 +1,9 @@
 'use server';
 
-import { Stuff, Item, Condition } from '@prisma/client';
+import { Item, Condition } from '@prisma/client';
 import { hash } from 'bcrypt';
 import { redirect } from 'next/navigation';
 import { prisma } from './prisma';
-
-/**
- * Adds a new stuff to the database.
- * @param stuff, an object with the following properties: name, quantity, owner, condition.
- */
-export async function addStuff(stuff: { name: string; quantity: number; owner: string; condition: string }) {
-  // console.log(`addStuff data: ${JSON.stringify(stuff, null, 2)}`);
-  let condition: Condition = 'good';
-  if (stuff.condition === 'poor') {
-    condition = 'poor';
-  } else if (stuff.condition === 'excellent') {
-    condition = 'excellent';
-  } else {
-    condition = 'fair';
-  }
-  await prisma.stuff.create({
-    data: {
-      name: stuff.name,
-      quantity: stuff.quantity,
-      owner: stuff.owner,
-      condition,
-    },
-  });
-  // After adding, redirect to the list page
-  redirect('/list');
-}
 
 export async function addItem(item: { name: string;
   condition: string; price: number; location: string; owner: string; imageUrl: string; description: string; }) {
@@ -57,25 +31,6 @@ export async function addItem(item: { name: string;
   redirect('/profile');
 }
 
-/**
- * Edits an existing stuff in the database.
- * @param stuff, an object with the following properties: id, name, quantity, owner, condition.
- */
-export async function editStuff(stuff: Stuff) {
-  // console.log(`editStuff data: ${JSON.stringify(stuff, null, 2)}`);
-  await prisma.stuff.update({
-    where: { id: stuff.id },
-    data: {
-      name: stuff.name,
-      quantity: stuff.quantity,
-      owner: stuff.owner,
-      condition: stuff.condition,
-    },
-  });
-  // After updating, redirect to the list page
-  redirect('/list');
-}
-
 export async function editItem(item: Item) {
   // console.log(`editItem data: ${JSON.stringify(item, null, 2)}`);
   await prisma.item.update({
@@ -92,19 +47,6 @@ export async function editItem(item: Item) {
   });
   // After updating, redirect to the list page
   redirect('/profile');
-}
-
-/**
- * Deletes an existing stuff from the database.
- * @param id, the id of the stuff to delete.
- */
-export async function deleteStuff(id: number) {
-  // console.log(`deleteStuff id: ${id}`);
-  await prisma.stuff.delete({
-    where: { id },
-  });
-  // After deleting, redirect to the list page
-  redirect('/list');
 }
 
 export async function deleteItem(id: number) {
--- src/lib/validationSchemas.ts
+++ src/lib/validationSchemas.ts
@@ -1,20 +1,5 @@
 import * as Yup from 'yup';
 
-export const AddStuffSchema = Yup.object({
-  name: Yup.string().required(),
-  quantity: Yup.number().positive().required(),
-  condition: Yup.string().oneOf(['excellent', 'good', 'fair', 'poor']).required(),
-  owner: Yup.string().required(),
-});
-
-export const EditStuffSchema = Yup.object({
-  id: Yup.number().required(),
-  name: Yup.string().required(),
-  quantity: Yup.number().positive().required(),
-  condition: Yup.string().oneOf(['excellent', 'good', 'fair', 'poor']).required(),
-  owner: Yup.string().required(),
-});
-
 export const AddItemSchema = Yup.object({
   name: Yup.string().required(),
   condition: Yup.string().oneOf(['excellent', 'good', 'fair', 'poor']).required(),
