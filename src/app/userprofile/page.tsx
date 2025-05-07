6f10607547843402f5d320a4b48d2241e0e4f928
Hargun Juneja <hargun@hawaii.edu>
2025-05-06 17:29:45 -1000

    Update page.tsx

--- src/app/userprofile/page.tsx
+++ src/app/userprofile/page.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import { Suspense } from 'react';
-import UserProfileContent from './UserProfileContent.tsx';
+import UserProfileContent from './UserProfileContent';
 
 export default function UserProfilePage() {
   return (
